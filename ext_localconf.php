a21f2fc1442ea2dab452132940757dd617c3005a
Benjamin Kott <benjamin.kott@wfp2.com>
2013-11-13 15:07:14 +0100

    hope nobody has seen this

--- ext_localconf.php
+++ ext_localconf.php
@@ -32,4 +32,45 @@ if(!defined ('TYPO3_MODE')){
 );
 
 
+/**
+ * Include TypoScript for tt_content before static
+ */
+$customFluidContentElementTypoScriptConstants = trim('
+plugin.tx_bk2kcontentelements {
+    view {
+        templateRootPath = EXT:bk2k_content_elements/Resources/Private/Templates/
+        partialRootPath = EXT:bk2k_content_elements/Resources/Private/Partials/
+        layoutRootPath = EXT:bk2k_content_elements/Resources/Private/Layouts/
+    }
+}
+');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    $_EXTKEY,
+    'constants',
+    $customFluidContentElementTypoScriptConstants
+);
+
+
+/**
+ * Include TypoScript for tt_content after static
+ */
+$customFluidContentElementTypoScriptSetup = trim('
+tt_content.bk2kcontentelements_customfluidelement = COA
+tt_content.bk2kcontentelements_customfluidelement {
+    10 = < lib.stdheader
+    20 = FLUIDTEMPLATE
+    20 {
+        file = {$plugin.tx_bk2kcontentelements.view.templateRootPath}CustomElement/Render.html
+        partialRootPath = {$plugin.tx_bk2kcontentelements.view.partialRootPath}
+        layoutRootPath = {$plugin.tx_bk2kcontentelements.view.layoutRootPath}
+    }
+}
+');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+    $_EXTKEY,
+    'setup',
+    $customFluidContentElementTypoScriptSetup,
+    43
+);
+
 ?>
